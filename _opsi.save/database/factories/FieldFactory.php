0f21ccc349135a54029333a3b2ebc5b816e16f8f
olivier-perrier <you@example.com>
2021-03-23 18:00:59 +0100

    authorizations doing

---
+++ _opsi.save/database/factories/FieldFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Field;
+use App\Models\PostType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class FieldFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Field::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'posttype_id' => PostType::factory()->make(),
+            'name' => $this->faker->name,
+            'type' => 'Text',
+        ];
+    }
+}
